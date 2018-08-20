faf6490342dd714a041cd0f14dc1bae323c05dd7
himanoa <matsunoappy@gmail.com>
2018-08-20 18:52:41 +0900

    feat: Implement user model

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
-extern crate diesel;
+#[macro_use] extern crate diesel;
 extern crate rocket;
 extern crate serde_json;
 #[macro_use] extern crate rocket_contrib;
 pub mod helpers;
 pub mod error_handlers;
+pub mod schema;
+pub mod models;
